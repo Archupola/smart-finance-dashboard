75807459acfdc1d866e1ff6c060b8080e85feb34
archupola <archanapola222@gmail.com>
2026-06-27 18:50:49 +0530

    Updated API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    "https://smart-finance-dashboard-api.onrender.com/api",
 });
 
-// Automatically attach JWT token to every request
 API.interceptors.request.use(
   (config) => {
     const user = JSON.parse(
